6cd7a2241dc1ebb7d1631dc376933a5b6ddc8af9
Michaela <michaelastewart1997@gmail.com>
2018-10-20 21:26:47 +0100

    Started profile app for users

--- Django_GraphicalPasswords/settings.py
+++ Django_GraphicalPasswords/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'GraphicalPasswords.apps.GraphicalpasswordsConfig',
+
+    #third party
+
+    #own apps
+    'profiles',
 ]
 
 MIDDLEWARE = [
