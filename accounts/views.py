c21eb4d442a40bde8c4f1fc5e57b1f83ea3a13b7
Michaela <michaelastewart1997@gmail.com>
2019-02-02 15:44:54 +0000

    Have creds on Login page but no form ??? Attempting to use the Django authenticate method to login user Need to move creds to a view function ???

--- accounts/views.py
+++ accounts/views.py
@@ -8,3 +8,17 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+class Login(generic.CreateView):
+    template_name = 'login_form.html'
+
+
+def get(self, request, *args, **kwargs):
+    context = locals()
+    context['username'] =
+
+    from django.contrib.auth import authenticate, login
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
