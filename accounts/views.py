24b92f1eeafcc062e5fd4cce601600400f5cb68a
muicheka <michaelastewart1997@gmail.com>
2019-02-16 16:30:18 +0000

    Working on recording user information while using application.

--- accounts/views.py
+++ accounts/views.py
@@ -28,6 +28,7 @@ def login_programmatically(request):
 class Signup(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
+
     template_name = 'registration/signup.html'
 
 
